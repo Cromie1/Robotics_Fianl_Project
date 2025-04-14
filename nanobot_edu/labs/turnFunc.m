beedf2c25c059519496580172cf1086f8b2a606e
dylan883 <Dylanhart883@gmail.com>
2025-04-14 13:44:47 -0600

    added turn func

---
+++ nanobot_edu/labs/turnFunc.m
@@ -0,0 +1,20 @@
+function turn(direction)
+if direction.equals('right')
+    nb.setMotor(1, 20);
+    nb.setMotor(2, -10);
+    
+    pause(0.58);
+    
+    nb.setMotor(1, 0);
+    nb.setMotor(2, 0);
+elseif direction.equals('left')
+    nb.setMotor(1, -10);
+    nb.setMotor(2, 20);
+    
+    pause(0.58);
+    
+    nb.setMotor(1, 0);
+    nb.setMotor(2, 0);
+else
+    return
+end
